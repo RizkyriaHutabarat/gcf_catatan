f19e2f544c63cfdc38a23c2c3179ba17847afd47
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-01-14 11:53:27 +0700

    updatefunction

---
+++ SignUp/function.go
@@ -0,0 +1,38 @@
+package gcf
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	module "github.com/RizkyriaHutabarat/be_tb/Module"
+)
+
+func init() {
+	functions.HTTP("Catatan", Catatan_register)
+}
+
+func Catatan_register(w http.ResponseWriter, r *http.Request) {
+	allowedOrigins := []string{"https://ksi-billboard.github.io", "http://127.0.0.1:5500", "http://127.0.0.1:5501"}
+	origin := r.Header.Get("Origin")
+
+	for _, allowedOrigin := range allowedOrigins {
+		if allowedOrigin == origin {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			break
+		}
+	}
+	// w.Header().Set("Access-Control-Allow-Origin", "https://intern-monitoring.github.io")
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	fmt.Fprintf(w, module.SignUpHandler("MONGOSTRING", "db_note", "user", r))
+
+}
